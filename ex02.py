a93dcce64b4caab807ab291371e8cf9f41bdb4a6
gschnirmann <gschnirmann@gmail.com>
2024-03-20 10:50:46 -0300

    Ex_02
    Versão final funcionando

---
+++ ex02.py
@@ -0,0 +1,15 @@
+#Calcular delta e avaliar raizes
+a = float(input("Informe a: "))
+b = float(input("Informe b: "))
+c = float(input("Informe c: "))
+
+#Cálulo do delta
+delta = b**2 - (4*a*c)
+#Avaliar as raízes
+if delta > 0:
+    print(f"Como delta é: {delta}, as raízes são reais e distintas ")
+elif delta == 0:
+    print(f"Como delta é: {delta}, as raízes são reais e iguais ")
+else:
+    print(f"Como delta é: {delta}, as raízes são imaginárias conjugadas ")
+
